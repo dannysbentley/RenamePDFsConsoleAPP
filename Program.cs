4632c9fc5e63967c63246c9c459773313fe7c563
Danny Bentley <danny.bentley@som.com>
2016-06-24 11:11:07 -0700

    Initial setup of Rename PDFs
    Application will auto rename PDFs and send to a folder

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RenamePDFSConsoleApp
+{
+    class RenameAndMove
+    {
+        static void Main(string[] args)
+        {
+            //Gets the directory path and directory to copy too.
+            string sourcePath = @"C:\Users\Danny.Bentley\Documents";
+            string targetPath = @"W:\S\BIM\_Current PDFs";
+
+            //check to see if the directory exists
+            //if (!(Directory.Exists(targetPath)))
+            //{
+            //    Console.WriteLine("This folder dose not exist");
+            //}
+
+            //Gets files from directory 
+            if (Directory.Exists(sourcePath))
+            {
+                string[] files = Directory.GetFiles(sourcePath);
+
+                foreach (string s in files)
+                {
+                    string fileName;
+                    string destfile;
+
+                    fileName = Path.GetFileName(s);
+
+                    //renames the file using regular expressions 
+                    Rename renameFile = new Rename();
+                    bool result = renameFile.IsPDFHeader(fileName);
+                    if (result == true)
+                    {
+                        renameFile.setFileName(fileName);
+
+                        renameFile.renamefile();
+                        string fileRename = renameFile.getFileName();
+
+                        //sets the new name to the old name
+                        string newName = fileRename;
+
+                        Console.WriteLine("File Renamed \n" + newName);
+
+                        //combines the new name to the directory 
+                        destfile = Path.Combine(targetPath, newName);
+
+                        //copies the files and will override the file if it already exists. 
+                        File.Copy(s, destfile, true);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Source path does not exist");
+            }
+
+            do
+            {
+                Console.WriteLine("Press x to exit >");
+            }
+            while (Console.ReadKey().KeyChar != 'x');
+        }
+
+        
+    }
+}
---
+++ Rename.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace RenamePDFSConsoleApp
+{
+    class Rename
+    {
+        private string fileName;
+        private string newFileName;
+        public Rename()
+        {
+        }
+
+        public void setFileName(string filename)
+        {
+            fileName = filename;
+        }
+
+        public string getFileName()
+        {
+            return newFileName;
+        }
+
+        public void renamefile()
+        {
+            try
+            {
+                string input;
+                var pattRegex = @"S\d.\d.*";
+                var fileNameRegex = Regex.Match(fileName, pattRegex, RegexOptions.None);
+                input = fileNameRegex.ToString();
+
+                //Seattle format. 
+                string renamedCorrect = ReplaceAt(input);
+                newFileName = renamedCorrect;
+                
+            }
+            catch { }
+        }
+
+        public bool IsPDFHeader(string fileName)
+        {
+            var pattRegex = @"\.(pdf)$";
+            var fileNameRegex = Regex.Match(fileName, pattRegex, RegexOptions.None);
+            if (fileNameRegex.ToString() == ".pdf")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static string ReplaceAt(string input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException("input");
+            }
+            char period = '.';
+            char[] chars = input.ToCharArray();
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (!char.IsWhiteSpace(chars[i]))
+                {
+                    if(chars[i] == '-')
+                    {
+                        chars[i] = period;
+                    }
+
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return new string(chars);
+        }
+
+    }
+}
